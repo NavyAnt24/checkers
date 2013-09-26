ab262feced36c476384ee1921989572a9f087d7b
App Academy Student <student@appacademy.io>
2013-09-26 12:05:37 -0700

    Working on Board#valid_move? method. Have check for jump moves and vectors for possible moves.

--- board.rb
+++ board.rb
@@ -1,6 +1,9 @@
 require './piece.rb'
 require 'colorize'
 
+class MoveError < StandardError
+end
+
 class Board
 
   def initialize
@@ -27,9 +30,9 @@ class Board
   end
 
   def print_board
-
+    puts
     @grid.length.times do |row|
-      print " #{row+1} "
+      print " #{row} " # can add 1 if you want columns to be 1 - 8
       @grid.length.times do |column|
         if (row + column) % 2 == 0
           background_color = :white
@@ -47,12 +50,12 @@ class Board
       end
       puts
     end
-  end
 
-  ### FIX COLUMN NUMBERS
+  print "   "
   8.times do |column_num|
-    print " #{column_num+1} "
+    print " #{column_num} " # can add 1 if you want columns to be 1 - 8
   end
+  puts
 
   end
 
@@ -61,17 +64,94 @@ class Board
   end
 
   def perform_slide(from_position, to_position)
-    if valid_move?Í(from_position, to_position)
+#    if valid_move?(from_position, to_position)
       piece = get_piece_at_position(from_position[0], from_position[1])
       @grid[from_position[0]][from_position[1]] = nil
       @grid[to_position[0]][to_position[1]] = piece
+#    end
+  end
+
+  def perform_jump(from_position, to_position)
+    # if valid_move?(from_position, to_position)
+      piece = get_piece_at_position(from_position[0], from_position[1])
+      @grid[from_position[0]][from_position[1]] = nil
+      @grid[to_position[0]][to_position[1]] = piece
+      between_position_row, between_position_column = position_in_between(from_position, to_position)
+      @grid[between_position_row][between_position_column] = nil
+    # end
+  end
+
+  def position_in_between(from_position, to_position)
+    row_difference = to_position[0] - from_position[0]
+    column_difference = to_position[1] - from_position[1]
+    between_position_row = from_position[0] + row_difference / 2
+    between_position_column = from_position[1] + column_difference / 2
+    return between_position_row, between_position_column
+  end
+
+  def valid_move?(from_position, to_position)
+    piece = get_piece_at_position(from_position[0], from_position[1])
+    possible_moves = possible_moves(from_position, piece)
+
+    if !get_piece_at_position[to_position[0], to_position[1]).nil?
+      raise MoveError "That position is occupied."
+      # return false
+    else
+      if jump_move?(from_position, to_position)
+        if get_piece_at_position(position_in_between(from_position, to_position)).nil?
+          raise MoveError "You cannot jump if there is no piece in between."
+          # return false
+      else
+        if !possible_moves(from_position).include?(to_position)
+        # return false
+      end
     end
+
+
   end
 
-  def perform_jump
-
+  def jump_move?(from_position, to_position)
+    row_difference = to_position[0] - from_position[0]
+    column_difference = to_position[1] - from_position[1]
+    if row_difference.abs == 2 || column_difference == 2
+      return true
+    end
+    false
   end
 
+  def vectors(piece)
+    non_king_red_vectors = [[-1,-1], [-1,1], [-2,-2], [-2,2]]
+    non_king_black_vectors = [[1,-1], [1,1], [2,-2], [2,2]]
 
+    if piece.king
+      vectors = non_king_red_vectors + non_king_black_vectors
+    else
+      if piece.color == :red
+        vectors = non_king_red_vectors
+      else
+        vectors = non_king_black_vectors
+      end
+    end
+    vectors
+  end
+
+  def possible_moves(from_position, piece)
+    vectors = vectors(piece)
+    possible_moves = []
+    vectors.each do |vector|
+      possible_position = from_position.zip(vector).map { |x,y| x+y }
+      if on_board?(possible_position)
+        possible_moves << from_position.zip(vector).map { |x,y| x+y }
+      end
+    end
+    possible_moves
+  end
+
+  def on_board?(position)
+    if position[0] < 0 || position [0] > 7 || position[1] < 0 || position[1] > 7
+      return false
+    end
+    true
+  end
 
 end
